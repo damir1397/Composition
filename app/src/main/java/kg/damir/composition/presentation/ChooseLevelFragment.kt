b09afca44ac35931630e32dd811855e140d6c76a
Damir <damir.danzo@gmail.com>
2022-08-23 22:34:38 +0600

    - jetpack navigation

--- app/src/main/java/kg/damir/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/kg/damir/composition/presentation/ChooseLevelFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import kg.damir.composition.R
 import kg.damir.composition.databinding.FragmentChooseLevelBinding
 import kg.damir.composition.domain.entity.Level
@@ -38,14 +39,15 @@ class ChooseLevelFragment : Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(GameFragment.NAME)
-            .commit()
+        val args = Bundle().apply {
+            putParcelable(GameFragment.KEY_LEVEL, level)
+        }
+
+        findNavController().navigate(R.id.action_chooseLevelFragment_to_gameFragment, args)
     }
 
     companion object {
-        const val NAME="ChooseLevelFragment"
+        const val NAME = "ChooseLevelFragment"
         fun newInstance(): ChooseLevelFragment {
             return ChooseLevelFragment()
         }
